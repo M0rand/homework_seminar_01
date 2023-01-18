d2405533bb92a7239509ec4866d1b56dedffa866
Vladimir Morar <morvladimir1981@gmail.com>
2023-01-18 21:51:18 +0300

    add example 04

---
+++ example04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberA = 0;
+while(numberA < numberB - 1)
+{
+    numberA += 2;
+    System.Console.Write(numberA + ", ");
+}
